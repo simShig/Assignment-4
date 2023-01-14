0fa59656abb107cf6fc284a7ecb67546d33b7745
simShig <simshigol@gmail.com>
2023-01-14 17:22:32 +0200

    completed repository construcctor

--- persistence.py
+++ persistence.py
@@ -56,6 +56,12 @@ class Repository(object):
         self._conn = sqlite3.connect('bgumart.db')
         self._conn.text_factory = bytes
         # tTODO: complete
+        self.employee = Dao(Employee,self._conn)
+        self.supplier = Dao(Supplier,self._conn)
+        self.product = Dao(Product,self._conn)
+        self.branches = Dao(Branche,self._conn)
+        self.activitie = Dao(Activitie,self._conn)
+        
         
     def _close(self):
         self._conn.commit()
@@ -106,67 +112,3 @@ atexit.register(repo._close)
 
 
 
-
-#Generic ORM and DAO:
-import inspect
- 
-def orm(cursor, dto_type):
- 
-    #the following line retrieve the argument names of the constructor
-    args = inspect.getargspec(dto_type.__init__).args
-    
-    #the first argument of the constructor will be 'self', it does not correspond 
-    #to any database field, so we can ignore it.
-    args = args[1:]  
- 
-    #gets the names of the columns returned in the cursor
-    col_names = [column[0] for column in cursor.description]
-    
-    #map them into the position of the corresponding constructor argument
-    col_mapping = [col_names.index(arg) for arg in args]
-    return [row_map(row, col_mapping, dto_type) for row in cursor.fetchall()]
- 
- 
-def row_map(row, col_mapping, dto_type):
-    ctor_args = [row[idx] for idx in col_mapping]
-    return dto_type(*ctor_args)
- 
- 
-#we can use our method above in order to start writing a generic Dao
-#note that this class is not complete and we will add methods to it next
-class Dao(object):
-    def __init__(self, dto_type, conn):
-        self._conn = conn
-        self._dto_type = dto_type
- 
-        #dto_type is a class, its __name__ field contains a string representing the name of the class.
-        self._table_name = dto_type.__name__.lower() + 's'
- 
-    def insert(self, dto_instance):
-        ins_dict = vars(dto_instance)
- 
-        column_names = ','.join(ins_dict.keys())
-        params = ins_dict.values()
-        qmarks = ','.join(['?'] * len(ins_dict))
- 
-        stmt = 'INSERT INTO {} ({}) VALUES ({})'\
-               .format(self._table_name, column_names, qmarks)
- 
-        self._conn.execute(stmt, params)
- 
-    def find_all(self):
-        c = self._conn.cursor()
-        c.execute('SELECT * FROM {}'.format(self._table_name))
-        return orm(c, self._dto_type)
-    
-    
-    def find(self, **keyvals):
-        column_names = keyvals.keys()
-        params = keyvals.values()
- 
-        stmt = 'SELECT * FROM {} WHERE {}' \
-               .format(self._table_name, ' AND '.join([col + '=?' for col in column_names]))
- 
-        c = self._conn.cursor()
-        c.execute(stmt, params)
-        return orm(c, self._dto_type)
